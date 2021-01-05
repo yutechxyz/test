02c74fada8b5f53945b7e9b05079ec016191107b
arakiyuto <araki@pyxos-jk.co.jp>
2021-01-05 17:45:43 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-btn
+      :x-large="false"
+      depressed
+      :color="color"
+      :disabled="false"
+      @click="handleClick"
+    >
+      Extra large Button
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: { default: 'warning' },
+    size: { default: 'x-large' }
+  },
+  methods: {
+    handleClick() {
+      console.log('click')
+    }
+  }
+}
+</script>
