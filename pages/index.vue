b3d425caa5e536d432aa1d26ec202af0dd18f2d7
arakiyuto <araki@pyxos-jk.co.jp>
2021-01-07 18:07:01 +0900

    aaa

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,35 @@
 <template>
   <v-row justify="center">
-    <v-dialog v-model="dialog" persistent max-width="600px">
+    <v-dialog v-model="dialog" persistent max-width="400px">
       <v-card>
-        <!-- タイトルは中央寄せにしたいね -->
         <v-card-title>
-          <span class="headline">Foot-Repo(ロゴ)</span>
+          <v-row justify="center">
+            Foot-Repo(ロゴ)
+          </v-row>
         </v-card-title>
         <v-card-text>
           <v-container>
             <v-row>
               <v-col cols="12" class="mb-n4">
                 <!-- margin-bottom -16px nが- -->
+                <!-- メールアドレスのフィールドはバリデーションが必要なので別コンポにする -->
                 <v-text-field
                   label="メールアドレス*"
+                  type="email"
                   required
-                ></v-text-field>
+                  v-model="email"
+                />
               </v-col>
               <v-col cols="12">
                 <v-text-field
                   label="パスワード*"
                   type="password"
                   required
-                ></v-text-field>
+                  v-model="pass"
+                />
               </v-col>
               <v-row justify="center" class="mt-4">
-                <v-col cols="6">
+                <v-col cols="8">
                   <v-btn
                     color="error"
                     block
@@ -39,7 +44,7 @@
           </v-container>
         </v-card-text>
         <v-card-actions>
-          <v-spacer></v-spacer>
+          <v-spacer />
           <v-btn
             color="info"
             text
@@ -62,7 +67,9 @@ export default {
   data() {
     return {
       dialog: true,
-      loading: false
+      loading: false,
+      email: '',
+      pass: ''
     }
   },
   methods: {
